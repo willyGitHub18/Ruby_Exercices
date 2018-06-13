add675daa295b185a70411c65505716e2c53f85c
willy <wilg1569@gmail.com>
2018-06-13 16:01:56 +0200

    Neuvième exercice Ruby

---
+++ exo_09.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, quel est ton prénom ?"
+print ">"
+user_name = gets.chomp
+puts "Merci, et puis-je connaître ton nom ?"
+print ">"
+user_surname = gets.chomp
+puts "Bonjour, #{user_name; user_surname}"
